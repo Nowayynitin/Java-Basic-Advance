24d8f5f625fa2d2191fb3c9641e4da5f4dcdbbfd
Nitin Patel <nitinpatel.cs@gmail.com>
2026-06-19 02:37:01 +0530

    Type Conversion

---
+++ Variables, Data Types & Input/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        float myFloat = 5;
+        System.out.println("My float is: " + myFloat);
+
+        int myInt = (int) 5.2f;
+        System.out.println("My integer is: " + myInt);
+    }
+}
